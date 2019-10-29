3b7166f0750b8331bf37a33c9cc97b4c9280d8a6
louiscklaw <louiscklaw@gmail.com>
2019-10-29 20:04:21 +0800

    update test and frozen the web page,.

--- test/test.js
+++ test/test.js
@@ -5,7 +5,25 @@ var fs = require('fs')
 
 var script_under_test = require(`${__dirname}/../main.js`)
 
-script_under_test.get_chinese_canidate_list(`https://www.elections.gov.hk/dc2019/pdf/nomination/CentralandWestern_20191018_c.html`)
+
+function test_parse_whole_hongkong_list(){
+  script_under_test.parse_whole_hongkong_list(`https://www.elections.gov.hk/dc2019/chi/nominat2.html`)
   .then(res => {
     console.log(res)
   })
+}
+
+
+function test_parse_canidate_list(){
+  script_under_test.get_chinese_canidate_list(`https://www.elections.gov.hk/dc2019/pdf/nomination/CentralandWestern_20191018_c.html`)
+  .then(res => {
+    console.log(res)
+  })
+}
+
+function test_all(){
+  test_parse_whole_hongkong_list()
+  test_parse_canidate_list()
+}
+
+test_all()
